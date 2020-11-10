2333c9f01d3dae73cfba803925637dbdac30708c
yunbchae <yunbchae@miridih.com>
2020-11-11 03:45:22 +0900

    CHAPTER2 스프링 부트에서 테스트 코드를 작성하자

---
+++ src/main/java/com/jojoldu/book/springboot/web/dto/HelloResponseDto.java
@@ -0,0 +1,25 @@
+package com.jojoldu.book.springboot.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+    private final String name;
+    private final int amount;
+
+    public HelloResponseDto(String name, int amount) {
+        this.name = name;
+        this.amount = amount;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+}
+
