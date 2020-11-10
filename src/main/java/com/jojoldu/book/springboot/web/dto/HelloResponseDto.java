c57b13d6419224aad2d6e4dc51b8c447e954ded4
yunbchae <yunbchae@miridih.com>
2020-11-11 04:06:45 +0900

    CHAPTER2 롬복 테스트에 적용안되는 오류 해결

--- src/main/java/com/jojoldu/book/springboot/web/dto/HelloResponseDto.java
+++ src/main/java/com/jojoldu/book/springboot/web/dto/HelloResponseDto.java
@@ -8,18 +8,5 @@ import lombok.RequiredArgsConstructor;
 public class HelloResponseDto {
     private final String name;
     private final int amount;
-
-    public HelloResponseDto(String name, int amount) {
-        this.name = name;
-        this.amount = amount;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getAmount() {
-        return amount;
-    }
 }
 
